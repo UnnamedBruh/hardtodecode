62894d787dd62bab6bd859691a6534f14d34fbd9
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-09-17 19:03:18 -0700

    Update hardtodecode.js

--- hardtodecode.js
+++ hardtodecode.js
@@ -7,8 +7,10 @@ function encode(text, key) {
 	encodeText = encodeText.replace(/./g, function(match) {
 		return match + Math.floor(i++ / 1000 % 10000).toString() + Math.floor(i / 100 % 1000).toString() + Math.floor(i / 10 % 100).toString() + Math.floor(i % 10).toString() + "\n"
 	})
-	encodeText = encodeText.split("\n").sort(() => Math.random() - 0.5).join("").replace(/[0-9]/g, function(match) {
-		return String.fromCharCode(match.charCodeAt(0) * 3)
-	})
+	encodeText = encodeText.split("\n").sort(() => Math.random() - 0.5).join("")
 	return encodeText
+}
+function decode(text, key) {
+	let shift = key.split("").map(item => item.charCodeAt(0)), i = 0
+	return text.match(/.([0-9]{4})/g).sort((i1, i2) => Number(i1.slice(1)) - Number(i2.slice(1))).map(match => String.fromCharCode(match.charCodeAt(0) - shift[(i = (i + 1) % shift.length)])).join("")
 }
